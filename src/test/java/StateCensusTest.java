f8d4cfe83c425aa914ffcfd085c66e65e93fb0de
its-shreybhatt <shreybhatt2@gmail.com>
2023-03-23 22:27:21 +0530

    Test Case 2 : Expected Exception due to Wrong CSV File

--- src/test/java/StateCensusTest.java
+++ src/test/java/StateCensusTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 public class StateCensusTest {
 
     public static final String INDIA_STATE_CENSUS_DATA = "./src/test/resources/IndiaStateCensusData.csv";
+    public static final String WRONG_CENSUS_DATA = ".//main/resources/IndiaStateCensusData.csv";
 
     @Test
     public void givenCensusData_CheckNumberOfRecords_AndPassTheTest()  {
@@ -11,9 +12,19 @@ public class StateCensusTest {
             IndianStateCensus indianStateCensus = new IndianStateCensus();
             int numOfRecords = indianStateCensus.loadIndiaCensusData(INDIA_STATE_CENSUS_DATA);
             Assert.assertEquals(29, numOfRecords);
-
         } catch (CensusAnalyserException e) {
             throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         }
+    }
+
+    @Test
+    public void givenWrongCensusData_CheckNumberOfRecords_ShouldThrowException()  {
+        try{
+            IndianStateCensus indianStateCensus = new IndianStateCensus();
+            indianStateCensus.loadIndiaCensusData(WRONG_CENSUS_DATA);
+        }catch (CensusAnalyserException e){
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+
     }
 }
